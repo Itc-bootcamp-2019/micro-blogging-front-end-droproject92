646b5dac4898f22bf437119fdd3695e3ce3a6fed
daniel.oren <orendaniel92@gmail.com>
2019-12-22 15:50:52 +0200

    Milestone 2 - catching server errors

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import TweetBox from "./TweetBox";
 import TweetMsg from "./TweetMsg";
 import { getTweetsList, postTweet } from "../lib/api";
-import { Ring } from 'react-awesome-spinners'
+import { Ring } from "react-awesome-spinners";
 
 class Home extends React.Component {
   constructor(props) {
@@ -14,14 +14,17 @@ class Home extends React.Component {
   }
 
   async handleData() {
-    this.setState({isLoading: true})
-    console.log(this.state.isLoading)
-    const response = await getTweetsList();
-    const { data } = response;
-    this.setState({
-      tweets: data.tweets,
-      isLoading: false
-    });
+    try {
+      this.setState({ isLoading: true });
+      const response = await getTweetsList();
+      const { data } = response;
+      this.setState({
+        tweets: data.tweets,
+        isLoading: false
+      });
+    } catch (e) {
+      console.error("Server Failed ;/");
+    }
   }
 
   componentDidMount() {
@@ -30,25 +33,34 @@ class Home extends React.Component {
   }
 
   async handleInput(userName, date, content) {
-    this.setState({isLoading:true});
-    const tweet = { userName, date, content };
-    await postTweet({ tweet: tweet });
-    this.setState({
-      tweets: [{ userName, date, content }, ...this.state.tweets],
-      isLoading: false
-    });
+    try {
+      this.setState({ isLoading: true });
+      const tweet = { userName, date, content };
+      await postTweet({ tweet: tweet });
+      this.setState({
+        tweets: [{ userName, date, content }, ...this.state.tweets],
+        isLoading: false
+      });
+    } catch (e) {
+      console.error("Server Failed ;/");
+    }
   }
 
   render() {
     const { tweets, isLoading } = this.state;
     return (
       <div>
-        <TweetBox isLoading={isLoading}
+        <TweetBox
+          isLoading={isLoading}
           onChange={(userName, date, content) =>
             this.handleInput(userName, date, content)
           }
         />
-        {isLoading && <div className="page-loader"><Ring /></div>}
+        {isLoading && (
+          <div className="page-loader">
+            <Ring />
+          </div>
+        )}
         {tweets.map(tweet => (
           <TweetMsg
             key={tweet.id}
